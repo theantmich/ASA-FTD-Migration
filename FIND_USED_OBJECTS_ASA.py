e7bd2f89c7852a65331e2e8f8b41dde759a67638
theantmich <61239471+theantmich@users.noreply.github.com>
2023-02-17 18:15:52 -0500

    Update FIND_USED_OBJECTS_ASA.py

--- FIND_USED_OBJECTS_ASA.py
+++ FIND_USED_OBJECTS_ASA.py
@@ -8,6 +8,8 @@ net_groups = []
 services = []
 svc_groups = []
 
+has_obj_nat = []
+
 used_objects = []
 unused_objects = []
 used_obj_groups = []
@@ -26,6 +28,21 @@ def to_netmask(cidr):
     netmask = socket.inet_ntoa(struct.pack('!I', (1 << 32) - (1 << host_bits)))
     return netmask
 
+def appendIfAbsent(obj,list):
+    if list.count(obj) > 0:
+        pass
+    else:
+        list.append(obj)
+    return
+
+def find_port(result):
+    if "eq" in result:
+        port = result[result.find('eq')+3:].rstrip()
+        return port
+    else:
+        print("PORT NOT EQUAL", result)
+        return
+    return
 
 def find_objects():
 
@@ -45,11 +62,7 @@ def find_objects():
 
                     obj = {"name": name, "ipv4": ipv4, "mask" : mask}
 
-                    objExists = objects.count(obj)
-                    if objExists > 0:
-                        pass
-                    else:
-                        objects.append(obj)
+                    appendIfAbsent(obj,objects)
 
                 elif nextLine[:6] == ' host ':
 
@@ -59,14 +72,18 @@ def find_objects():
 
                     obj = {"name": name, "ipv4": ipv4, "mask" : mask}
 
-                    objExists = objects.count(obj)
+                    appendIfAbsent(obj,objects)
+                                        
+                elif nextLine[:4] == ' nat':
+
+                    name = line[15:].rstrip()
+
+                    # Do not replace with appendIfAbsent method, appending only name and not the whole object
+                    objExists = has_obj_nat.count(obj)
                     if objExists > 0:
                         pass
                     else:
-                        objects.append(obj)
-                    
-                elif nextLine[:4] == ' nat':
-                    pass
+                        has_obj_nat.append(name)
                 
             elif not line:
                 break
@@ -83,35 +100,32 @@ def find_services():
                 nextLine = next(file, '').rstrip()
 
                 if nextLine[13:24] == 'destination':
-
+                    type = 'destination'
                     name = line[15:].strip()
-                    proto = nextLine[9:12]
+                    protocol = nextLine[9:12]
 
                     if 'range' in nextLine:
                         port = nextLine[nextLine.find('range')+6:]
                     else:
                         port = nextLine[28:]
                     
-                    obj = {"name": name, "proto": proto, "port" : port}
+                    obj = {"name": name, "protocol": protocol, "port" : port, 'type' : type}
 
-                    services.append(obj)
+                    appendIfAbsent(obj,services)
 
                 elif nextLine[13:19] == 'source':
+                    type = 'source'
                     name = line[15:].strip()
-                    proto = nextLine[9:12]
+                    protocol = nextLine[9:12]
                     
                     if 'range' in nextLine:
                         port = nextLine[nextLine.find('range')+6:]
                     else:
                         port = nextLine[23:]
 
-                    obj = {"name": name, "protocol": proto, "port" : port}
+                    obj = {"name": name, "protocol": protocol, "port" : port, 'type' : type}
                     
-                    objExists = services.count(obj)
-                    if objExists > 0:
-                        pass
-                    else:
-                        services.append(obj)
+                    appendIfAbsent(obj,services)
                 
             elif not line:
                 
@@ -152,11 +166,7 @@ def find_object_groups():
 
                 obj = {"name": name, "ipv4": ipv4, "mask" : mask}
 
-                objExists = objects.count(obj)
-                if objExists > 0:
-                    pass
-                else:
-                    objects.append(obj)
+                appendIfAbsent(obj,objects)
                 
                 new_children.append(name)
             else:
@@ -170,7 +180,7 @@ def find_object_groups():
             'children' : children
         }
 
-        net_groups.append(group)
+        appendIfAbsent(group,net_groups)
     #print(net_groups)
     return
     
@@ -190,82 +200,62 @@ def find_service_groups():
         for i in range(len(result)):
             if "destination" in result[i]:
                 decalage = 11
+                type = "destination"
             elif "source" in result[i]:
                 decalage = 6
+                type = 'source'
 
             if "service-object object" in result[i]:
                 child = result[i][23:]
-                new_children.append(child)
+                new_children.append(child.rstrip())
 
             elif "service-object tcp-udp" in result[i]:
+                port = find_port(result[i])
                 
-                if "eq" in result[i]:
-                    port = result[i][result[i].find('eq')+3:].rstrip()
-                else:
-                    print("PORT NOT EQUAL", result[i])
-                    break
-
-                proto = "TCP-UDP"
-                name = "SVC_"+ proto + "_"+ port
+                protocol = "TCP-UDP"
+                name = "INLINE_"+ protocol + "_"+ port
                 child = {
                     'name' : name,
-                    'protocol' : proto,
-                    'port' : port
+                    'protocol' : protocol,
+                    'port' : port,
+                    'type' : type
                 }
 
-                objExists = services.count(child)
-                if objExists > 0:
-                    pass
-                else:
-                    services.append(child)
+                appendIfAbsent(child,services)
 
                 new_children.append(name)
 
             elif "service-object tcp" in result[i]:
                 
-                if "eq" in result[i]:
-                    port = result[i][result[i].find('eq')+3:].rstrip()
-                else:
-                    print("PORT NOT EQUAL", result[i])
-                    break
+                port = find_port(result[i])
 
-                proto = "TCP"
-                name = "SVC_"+ proto + "_"+ port
+                protocol = "TCP"
+                name = "INLINE_"+ protocol + "_"+ port
                 child = {
                     'name' : name,
-                    'protocol' : proto,
-                    'port' : port
+                    'protocol' : protocol,
+                    'port' : port,
+                    'type' : type
                 }
 
-                objExists = services.count(child)
-                if objExists > 0:
-                    pass
-                else:
-                    services.append(child)
+                appendIfAbsent(child,services)
 
                 new_children.append(name)
 
             elif "service-object udp" in result[i]:
                 
-                if "eq" in result[i]:
-                    port = result[i][result[i].find('eq')+3:].rstrip()
-                else:
-                    print("PORT NOT EQUAL", result[i])
-                    break
+                port = find_port(result[i])
 
-                proto = "UDP"
-                name = "SVC_"+ proto + "_"+ port
+                protocol = "UDP"
+                name = "INLINE_"+ protocol + "_"+ port
                 child = {
                     'name' : name,
-                    'protocol' : proto,
-                    'port' : port
+                    'protocol' : protocol,
+                    'port' : port,
+                    'type' : type
                 }
 
-                objExists = services.count(child)
-                if objExists > 0:
-                    pass
-                else:
-                    services.append(child)
+                appendIfAbsent(child,services)
 
                 new_children.append(name)
 
@@ -273,15 +263,13 @@ def find_service_groups():
                 pass
 
         children=new_children
-        #print(groupName,children)
         
         group = {
             'name' : groupName,
             'children' : children
         }
 
-        svc_groups.append(group)
-    #print(net_groups)
+        appendIfAbsent(group,svc_groups)
     return
 
 def is_grp_used():
@@ -294,10 +282,9 @@ def is_grp_used():
         objExists = len(hits) - false_hits
 
         if objExists > 0:
-            used_obj_groups.append(group)
-
+            appendIfAbsent(group,used_obj_groups)
         else:
-            unused_obj_groups.append(group)
+            appendIfAbsent(group,unused_obj_groups)
 
     for group in svc_groups:
         parse = CiscoConfParse("../show_run.txt")
@@ -307,11 +294,10 @@ def is_grp_used():
         
         objExists = len(hits) - false_hits
 
-
         if objExists > 0:
-            used_svc_groups.append(group)
+            appendIfAbsent(group,used_svc_groups)
         else:
-            unused_svc_groups.append(group)
+            appendIfAbsent(group,unused_svc_groups)
 
     return
 
@@ -322,38 +308,93 @@ def is_obj_used():
             count = group['children'].count(obj['name'])
 
             if count > 0:
-
-                objInList = used_objects.count(obj)
-                if objInList > 0:
-                    pass
-                else:
-                    used_objects.append(obj)
+                appendIfAbsent(obj,used_objects)
                 #Otherwise, check if object is in unused groups
             else:
                 #print(obj)
                 #print(unused_obj_groups)
-                for ugroup in unused_obj_groups:
-                    known_appearances = ugroup['children'].count(obj['name']) + 1 + count
-                    #print(known_appearances, obj)
 
-                    parse = CiscoConfParse("../show_run.txt")
-                    hits = parse.find_objects(obj['name'])
+                parse = CiscoConfParse("../show_run.txt")
+                hits = parse.find_objects(obj['name'])
+                
+                false_hits = str(hits).split(" ").count("'object") + str(hits).split(" ").count("'object-group") + str(hits).split(" ").count("'network-object")
+
+                #print(obj, len(hits), false_hits, known_appearances)
+
+                if len(hits)-false_hits > 0:
+                    appendIfAbsent(obj,used_objects)
+
+                elif "INLINE_" in obj['name']:
+                    ipv4 = obj['ipv4']
+                    mask = to_netmask(obj['mask'])
+                    to_search = ipv4 + " " + mask
+
+                    hits_inline = parse.find_objects(to_search)
                     
-                    if len(hits) > known_appearances:
-                        objInList = used_objects.count(obj)
-                        if objInList > 0:
-                            pass
-                        else:
-                            used_objects.append(obj)
+                    if len(hits_inline) > 0:
+                        appendIfAbsent(obj,used_objects)
+
+                elif has_obj_nat.count(obj['name']) > 0:
+                    appendIfAbsent(obj,used_objects)
+                else:
+                    appendIfAbsent(obj,unused_objects)
+
+                #print(hits, obj)
+
+    return
+
+def is_svc_used():
+        #Check if object is in used group
+    for svc in services:
+        for group in used_svc_groups:
+
+            parse = CiscoConfParse("../show_run.txt")
+            hits = parse.find_objects(svc['name'])
+
+            count = group['children'].count(svc['name'])
+
+            if count > 0:
+                #print(svc, used_svc_groups)
+                appendIfAbsent(svc,used_services)
+                #print(svc, "REASON : IN USED GROUP", hits)
+            else:
+                name = svc['name']
+                p_type = svc['type']
+                protocol = svc['protocol']
+                port = svc['port']
+
+                srv_prot_type_port = "service "+ protocol + " " + p_type + " eq " + port
+                grp_prot_type_port = "service-object " + protocol + " " + p_type + " eq " + port
+                grp_srv_name = "object-group service " + name
+                obj_srv_name = "object service " + name
+                srv_obj_name = "service-object " + name
+
+                false_hits_1 = re.findall(srv_prot_type_port, str(hits))
+                false_hits_2 = re.findall(grp_prot_type_port, str(hits))
+                false_hits_3 = re.findall(grp_srv_name, str(hits))
+                false_hits_4 = re.findall(obj_srv_name, str(hits))
+                false_hits_5 = re.findall(srv_obj_name, str(hits))
+
+                total_false_hits = len(false_hits_1) + len(false_hits_2) + len(false_hits_3) + len(false_hits_4) + len(false_hits_5)
+
+                if len(hits) > total_false_hits:
+                    appendIfAbsent(svc,used_services)
+                    #print(svc, "REASON : IN CONFIG", hits, len(hits), total_false_hits)
+                elif "INLINE_" in svc['name']:
+                    to_search = protocol + " " + p_type + " eq " + port
+                    to_search2 = "eq " + port
+
+                    hits_inline = parse.find_objects(to_search)
+                    hits_inline2 = parse.find_objects(to_search2)
+
+                    if len(hits_inline)+len(hits_inline2) > 0:
+                        appendIfAbsent(svc,used_services)
+                        #print(svc, "REASON : INLINE USED")
                     else:
-                        objInList = unused_objects.count(obj)
-                        if objInList > 0:
-                            pass
-                        else:
-                            unused_objects.append(obj)
-
-                    #print(hits, obj)
-
+                        appendIfAbsent(svc,unused_services)
+                        #print(svc, "REASON : NOT USED")
+                else:
+                    appendIfAbsent(svc,unused_services)
     return
 
 find_objects()
@@ -363,16 +404,46 @@ find_service_groups()
 
 is_grp_used()
 is_obj_used()
+is_svc_used()
 
-#print(objects)
-#print(net_groups)
-#print(services)
-#print(svc_groups)
-
+print("############## USED OBJECT GROUPS ##################")
 print(used_obj_groups)
-print("################################")
+print("#######################################################")
+print("Groupes d'objets utilisés :", len(used_obj_groups))
+print("############## UNUSED OBJECT GROUPS ##################")
 print(unused_obj_groups)
+print("#######################################################")
+print("Groupes d'objets inutilisés :", len(unused_obj_groups))
+print("Groupes d'objets totaux :", len(net_groups))
 
+print("############### USED OBJECTS #################")
 print(used_objects)
-print("################################")
+print("#######################################################")
+print("Objets utilisés :", len(used_objects))
+print("############### UNUSED OBJECTS #################")
 print(unused_objects)
+print("#######################################################")
+print("Objets inutilisés :", len(unused_objects))
+print("Objets totaux :", len(objects))
+
+print("############## USED SERVICES GROUPS ##################")
+print(used_svc_groups)
+print("#######################################################")
+print("Groupes de services utilisés :", len(used_svc_groups))
+print("############## UNUSED SERVICE GROUPS ##################")
+print(unused_svc_groups)
+print("#######################################################")
+print("Groupes de services inutilisés :", len(unused_svc_groups))
+print("Groupes de services totaux :", len(svc_groups))
+
+print("############### USED SERVICES #################")
+print(used_services)
+print("#######################################################")
+print("Services utilisés :", len(used_services))
+print("############### UNUSED SERVICES #################")
+print(unused_services)
+print("#######################################################")
+print("Services inutilisés :", len(unused_services))
+print("################ SERVICES ###################")
+print(services)
+print("Services totaux :", len(services))
